16c1f6a99d859027efc9f7b4ad0870f05ae0ce8e
gaowenfeng02 <gaowenfeng02@meituan.com>
2018-05-16 13:02:46 +0800

    项目初始化

---
+++ src/main/java/com/acmen/acmenhelper/AcmenHelperApplication.java
@@ -0,0 +1,12 @@
+package com.acmen.acmenhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmenHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmenHelperApplication.class, args);
+    }
+}
